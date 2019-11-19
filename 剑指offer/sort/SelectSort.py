b4af375924ee7c50a7f5865fbda6c4ef7ac61695
Tommy <zhaoliang4113@gmail.com>
2019-11-19 19:50:34 +0800

    test

--- 剑指offer/sort/SelectSort.py
+++
@@ -1,9 +0,0 @@
-def selecrSort(arr):
-	for i in range(len(arr)-1):
-		minIndex = i
-		for j in range(i+1, len(arr)):
-			if arr[j] < arr[minIndex]:
-				minIndex = j
-		if i != minIndex:
-			arr[i], arr[minIndex] = arr[i], arr[minIndex]
-	return arr
